c92d9ce6471d93ee9c4f1588ba15a1c289552e6f
suhana <diundisuhana@gmail.com>
2025-08-02 08:26:15 +0530

    Delete Q13-17 directory

--- Q13-17/Info.java
+++
@@ -1,9 +0,0 @@
-public class Info {
-    public static void main(String[] args){
-        Student s1 = new Student();
-        s1.name = "Suhana";
-        s1.age = 18;
-
-        s1.printInfo(s1.name, s1.age);
-    }
-}
--- Q13-17/color.java
+++
@@ -1,9 +0,0 @@
-public class color {
-    public static void main(String[] args){
-
-        Triangle t1 = new Triangle();
-        t1.color = "red";
-
-        t1.printInfo(t1.color);
-    }
-}
