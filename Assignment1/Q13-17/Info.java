58ed4132d86956533f93a29bc09e9056eb072b2d
suhana <diundisuhana@gmail.com>
2025-08-02 08:28:19 +0530

    Add files via upload

---
+++ Assignment1/Q13-17/Info.java
@@ -0,0 +1,9 @@
+public class Info {
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.name = "Suhana";
+        s1.age = 18;
+
+        s1.printInfo(s1.name, s1.age);
+    }
+}
---
+++ Assignment1/Q13-17/shape.java
@@ -0,0 +1,13 @@
+//inheritence
+
+class shape {
+
+    String color;
+
+}
+
+class Triangle extends shape{
+    public void printInfo(String color){
+        System.out.println(color);
+    }
+}
