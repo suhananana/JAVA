95e8d44048fa6830756bcef143d07b887d2b5f1a
suhana <diundisuhana@gmail.com>
2025-08-02 08:25:02 +0530

    Delete Q1_to_Q12 directory

--- Q1_to_Q12/TestMultipleInheritence.java
+++
@@ -1,7 +0,0 @@
-public class TestMultipleInheritence {
-    public static void main(String[] args) {
-        Duck d = new Duck();
-        d.fly();
-        d.swim();
-    }
-}
--- Q1_to_Q12/TestPolymorphism.java
+++
@@ -1,7 +0,0 @@
-public class TestPolymorphism {
-    public static void main(String[] args) {
-        Animal a;
-        a = new Cat(); a.makeSound();
-        a = new Cow(); a.makeSound();
-    }
-}
