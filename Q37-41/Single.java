6a48a6184cdbf73def108072716122cc61c75633
suhana <diundisuhana@gmail.com>
2025-08-02 08:25:49 +0530

    Delete Q37-41 directory

--- Q37-41/Single.java
+++
@@ -1,8 +0,0 @@
-public class Single {
-    public static void main(String[] args) {
-        Singleton s1 = Singleton.getInstance();
-        Singleton s2 = Singleton.getInstance();
-
-            System.out.println("s1 and s2 are the same object: " + (s1 == s2));
-        }
-}
