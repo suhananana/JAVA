8cb93b67440f409a37f3e7158d45518865fb9e1b
suhana <diundisuhana@gmail.com>
2025-08-02 08:25:57 +0530

    Delete Q32-36 directory

--- Q32-36/StringComparison.java
+++
@@ -1,9 +0,0 @@
-public class StringComparison {
-    public static void main(String[] args) {
-        String str1 = new String("Hello");
-        String str2 = new String("Hello");
-
-        System.out.println("Using == : " + (str1 == str2));           // false
-        System.out.println("Using equals(): " + str1.equals(str2));   // true
-    }
-}
