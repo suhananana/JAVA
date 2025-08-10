f2aa72a7a1db5e978bb173ca2b59883d2bd16489
suhana <diundisuhana@gmail.com>
2025-08-10 11:42:10 +0530

    Add files via upload

---
+++ Assignment2/SEC1/Q1/Math.java
@@ -0,0 +1,9 @@
+import utilities.MathUtils;
+public class Math {
+
+    public static void main(String[] args){
+        MathUtils math = new MathUtils();
+        int result = math.add(10,20);
+        System.out.println("Sum : " + result);
+    }
+}
