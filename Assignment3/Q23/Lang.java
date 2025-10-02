0c262e125afe95be07e083e98f46c4d4186b718a
suhana <diundisuhana@gmail.com>
2025-10-02 10:09:20 +0530

    Add files via upload

---
+++ Assignment3/Q23/Lang.java
@@ -0,0 +1,7 @@
+public class Lang {
+    public static void main(String[] args) {
+        System.out.println("Random number: " + Math.random());
+        System.out.println("Absolute value: " + Math.abs(-25));
+        System.out.println("Power: " + Math.pow(2, 5));
+    }
+}
