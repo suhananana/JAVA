e10b7797f63d6685972905115dd65f8c198183d5
suhana <diundisuhana@gmail.com>
2025-10-11 11:27:26 +0530

    Add files via upload

---
+++ MOD3 SET3/Q4/MathFunctionExample.java
@@ -0,0 +1,11 @@
+public class MathFunctionsExample {
+    public static void main(String[] args) {
+        double randomValue = Math.random();
+        int absValue = Math.abs(-25);
+        double powerValue = Math.pow(2, 5);
+
+        System.out.println("Random Value: " + randomValue);
+        System.out.println("Absolute Value: " + absValue);
+        System.out.println("Power Value (2^5): " + powerValue);
+    }
+}
